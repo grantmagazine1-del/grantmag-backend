fe488ca794eb8a3646f5fef5c66c0a21f98a09ee
everythingcover <noahgrahambrown@gmail.com>
2026-03-05 15:18:35 -0800

    firebase admin key fetching

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const parser = new Parser();               // RSS parser
 const PORT = process.env.PORT || 3000;     // server port
 const FEED_URL = "https://grantmagazine.com/feed/"; // fetching URL for magazine data
 
+const admin = require("firebase-admin"); // firebase admin for push notif
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  })
+});
+
+const db = admin.firestore();
+const messaging = admin.messaging();
+
 app.use(cors()); // cors for browser testing security
 
 let cachedFeed = null;
