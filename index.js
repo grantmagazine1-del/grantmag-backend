a8e39b58fb8583eeac8f36fdf510a08d8f09f99f
everythingcover <noahgrahambrown@gmail.com>
2026-03-05 15:20:54 -0800

    variable name issues

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const FEED_URL = "https://grantmagazine.com/feed/"; // fetching URL for magazine
 const admin = require("firebase-admin"); // firebase admin for push notif
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
+    projectId: process.env.FIREBASE_PROJ_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: process.env.FIREBASE_PRIV_KEY.replace(/\\n/g, '\n'),
   })
 });
 
